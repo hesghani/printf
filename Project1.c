1cafa85fa1b66dafb122516db3e1a9d3021d6a1f
hesghani <hesghani@gmail.com>
2023-09-16 14:25:38 -0700

    4th try is a charm

--- Project1.c
+++ Project1.c
@@ -3,52 +3,59 @@
 #include <unistd.h>
 #include "main.h"
 
-static void print_char(int c, int *count) {
-write(1, &c, 1);
-(*count)++;
-}
-
-static void print_str(const char *str, int *count) {
-if (str == NULL)
-str = "(null)";
-while (*str) {
-write(1, str, 1);
-str++;
-(*count)++;
-}
-}
-
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
 int count = 0;
 va_list args;
 
 va_start(args, format);
 
-while (*format) {
-if (*format != '%') {
+while (*format)
+{
+if (*format != '%')
+{
 /* If not a format specifier, print the character */
-print_char(*format, &count);
-} else {
+write(1, format, 1);
+count++;
+}
+else
+{
 format++; /* Move past '%' */
 
 if (*format == '\0')
 break; /* Format string ends with '%', stop processing */
 
-if (*format == 'c') {
+if (*format == 'c')
+{
 /* Handle character specifier */
 int c = va_arg(args, int);
-print_char(c, &count);
-} else if (*format == 's') {
+write(1, &c, 1);
+count++;
+}
+else if (*format == 's')
+{
 /* Handle string specifier */
 char *str = va_arg(args, char *);
-print_str(str, &count);
-} else if (*format == '%') {
+if (str == NULL)
+str = "(null)";
+while (*str)
+{
+write(1, str, 1);
+str++;
+count++;
+}
+}
+else if (*format == '%')
+{
 /* Handle '%' specifier */
-print_char('%', &count);
-} else {
+write(1, "%", 1);
+count++;
+}
+else
+{
 /* Invalid format specifier, ignore it */
-print_char('%', &count); /* Print the '%' character */
-print_char(*format, &count); /* Print the invalid character */
+write(1, "%", 1); /* Print the '%' character */
+write(1, format, 1); /* Print the invalid character */
 count += 2;
 }
 }
@@ -56,5 +63,6 @@ format++;
 }
 
 va_end(args);
-return count;
+return (count);
 }
+
