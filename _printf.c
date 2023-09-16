25a19eec06eefc7cedf09243e05b64e9abf49949
hesghani <hesghani@gmail.com>
2023-09-16 15:09:38 -0700

    pllllllllllllllllzzzz

--- _printf.c
+++ _printf.c
@@ -1,8 +1,17 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include "main.h"
 
+/* Function prototypes */
+int print_char(int c);
+int print_str(char *str);
+
+/**
+ * _printf - Our custom printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
 int count = 0;
@@ -14,48 +23,29 @@ while (*format)
 {
 if (*format != '%')
 {
-/* If not a format specifier, print the character */
 write(1, format, 1);
 count++;
 }
 else
 {
-format++; /* Move past '%' */
+format++;
 
 if (*format == '\0')
-break; /* Format string ends with '%', stop processing */
+break;
 
 if (*format == 'c')
-{
-/* Handle character specifier */
-int c = va_arg(args, int);
-write(1, &c, 1);
-count++;
-}
+count += print_char(va_arg(args, int));
 else if (*format == 's')
-{
-/* Handle string specifier */
-char *str = va_arg(args, char *);
-if (str == NULL)
-str = "(null)";
-while (*str)
-{
-write(1, str, 1);
-str++;
-count++;
-}
-}
+count += print_str(va_arg(args, char *));
 else if (*format == '%')
 {
-/* Handle '%' specifier */
 write(1, "%", 1);
 count++;
 }
 else
 {
-/* Invalid format specifier, ignore it */
-write(1, "%", 1); /* Print the '%' character */
-write(1, format, 1); /* Print the invalid character */
+write(1, "%", 1);
+write(1, format, 1);
 count += 2;
 }
 }
@@ -63,6 +53,41 @@ format++;
 }
 
 va_end(args);
+return (count);
+}
+
+/**
+ * print_char - Print a character.
+ * @c: The character to print.
+ *
+ * Return: 1 (the number of characters printed).
+ */
+int print_char(int c)
+{
+write(1, &c, 1);
+return (1);
+}
+
+/**
+ * print_str - Print a string.
+ * @str: The string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_str(char *str)
+{
+int count = 0;
+
+if (str == NULL)
+str = "(null)";
+
+while (*str)
+{
+write(1, str, 1);
+str++;
+count++;
+}
+
 return (count);
 }
 
